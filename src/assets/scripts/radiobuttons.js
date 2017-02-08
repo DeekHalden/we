c01bb0c62c88eabd88f9600285f87db326a67864
deekhalden <iliy4@ua.fm>
2017-02-08 18:27:22 +0200

    better

--- src/assets/scripts/radiobuttons.js
+++ src/assets/scripts/radiobuttons.js
@@ -3,7 +3,3 @@ var inputList = $('.radio-input');
 for (var i = inputList.length - 1; i >= 0; i--) {
     $(inputList[i]).prettyCheckable();
 }
-
-
-var input = $('.checkbox');
-input.prettyCheckable();
